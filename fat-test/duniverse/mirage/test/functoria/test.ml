0fefd4ef0797e29271e4af62abb8dbbbc734893f
PizieDust <playersrebirth@gmail.com>
2023-07-04 10:08:58 +0100

    remove build files

--- fat-test/duniverse/mirage/test/functoria/test.ml
+++
@@ -1,9 +0,0 @@
-let () =
-  Alcotest.run "functoria"
-    [
-      ("cli", Test_cli.suite);
-      ("package", Test_package.suite);
-      ("graph", Test_graph.suite);
-      ("action", Test_action.suite);
-      ("key", Test_key.suite);
-    ]
--- fat-test/duniverse/ocaml-fat/TODO.md
+++
@@ -1,4 +0,0 @@
-* add concurrency monad
-* use the result monad properly
-* be consistent about sector size (replace 512 with bytes_per_sector)
-
